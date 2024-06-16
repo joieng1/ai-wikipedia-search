1878ef5397104132f999f64adcd9c4a6f9874cad
joieng1 <johncieng1@gmail.com>
2024-06-16 16:46:55 -0700

    implement greedyBFS and main route to call search

--- src/app/api/wikipedia/route.ts
+++ src/app/api/wikipedia/route.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
 import { pipeline } from "@xenova/transformers";
+import { PriorityQueue } from "@/lib/PriorityQueue";
 
 const extractor = await pipeline(
   "feature-extraction",
@@ -52,19 +53,17 @@ async function compareTwoWords(word1: string, word2: string) {
 async function getLinks(title: string) {
   try {
     const getResponse = await fetch(
-      `https://en.wikipedia.org/w/api.php?action=query&titles=${title}&prop=links&pllimit=max&format=json`
+      `https://en.wikipedia.org/w/api.php?action=query&titles=${encodeURIComponent(title)}&prop=links&pllimit=max&format=json`
     );
     const data: wikipediaRes = await getResponse.json();
 
     const pageId: string = Object.keys(data.query.pages)[0];
-    //handle missing links
     const page = data.query.pages[pageId];
     if (pageId === "-1" || page.missing !== undefined) {
-      // Page is missing or doesn't exist
       console.warn(`Page "${title}" is missing or does not exist.`);
       return [];
     }
-    const links = data.query.pages[pageId].links;
+    const links = page.links;
     const filteredLinks = links
       .filter((link) => link.ns === 0)
       .map((link) => link.title);
@@ -72,5 +71,58 @@ async function getLinks(title: string) {
     return filteredLinks;
   } catch (error: any) {
     throw error;
+  }
+}
+
+async function greedyBFS(
+  startWord: string,
+  targetWord: string,
+): Promise<string[]> {
+  const visited = new Set<string>(); // Set to keep track of visited pages
+  const priorityQueue = new PriorityQueue<{ word: string; path: string[] }>();
+  console.log(targetWord);
+  priorityQueue.enqueue({ word: startWord, path: [startWord] }, 0);
+
+  while (!priorityQueue.isEmpty()) {
+    const { word: currentWord, path: currentPath } = priorityQueue.dequeue()!;
+    if (visited.has(currentWord)) continue;
+    
+    visited.add(currentWord); // Marking the current page as visited
+    console.log(`Current page: ${currentWord}`);
+
+    if (currentWord.toLowerCase() === targetWord.toLowerCase()) {
+      return currentPath;
+    }
+    
+    const links = await getLinks(currentWord);
+    for (const link of links) {
+      if (visited.has(link)) continue; // Skip already visited links
+      const similarity = await compareTwoWords(link, targetWord);
+      
+      const newPath = [...currentPath, link];
+      priorityQueue.enqueue({ word: link, path: newPath }, similarity);
+    }
+  }
+
+  throw new Error('No path to target page found. The target may not be reachable.');
+}
+
+
+export async function GET(req: NextRequest) {
+  const title = req.nextUrl.searchParams.get("title");
+  const targetWord = req.nextUrl.searchParams.get("targetWord");
+  if (title === null || targetWord === null) {
+    return new NextResponse(JSON.stringify({ Error: "Title or target word missing" }), {
+      status: 400,
+    });
+  }
+  try {
+    const result = await greedyBFS(title, targetWord);
+    return NextResponse.json(result);
+  } catch (error: unknown) {
+    console.error("Error calling API", error);
+    return NextResponse.json({
+      message: "Failed to fetch data from Wikipedia",
+    });
   }
 }
